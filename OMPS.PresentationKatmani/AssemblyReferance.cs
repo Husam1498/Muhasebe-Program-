4e9509ed016920f72e19068b58ba688af0d21957
Hüsamettin demir <husamettin1498@gmail.com>
2025-10-08 17:13:29 +0300

    Add project files.

---
+++ OMPS.PresentationKatmani/AssemblyReferance.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace OMPS.PresentationKatmani
+{
+    /// <summary>
+    /// bu assembly i oluşturmamızın amacı web api tarafında
+    /// oluşacak olan controllerı presentatiion katmanına taşımamızdan kayunaklıdır.
+    /// controller sınıfını web api katmanında tanıtmamız için bu gereklidir
+    public static  class AssemblyReferance
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReferance).Assembly; 
+
+    }
+}
---
+++ OMPS.WebApi/Program.cs
@@ -0,0 +1,39 @@
+
+using OMPS.PresentationKatmani;
+
+namespace OMPS.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers()
+                .AddApplicationPart(typeof(AssemblyReferance).Assembly);// presentation katmanýndaki controllerlarý tanýtmak için
+          
+            
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
