a654b554fcd5de8ac9a4600f814cf12ff9d6b47d
Hüsamettin demir <husamettin1498@gmail.com>
2025-11-04 14:41:26 +0300

    MainRoleAndRoleRelationship için Delete işlemi için Validator yapıldı

---
+++ OnlineMuhasebeProgramıServer.ApplicationKatmanı/Features/Commands/AppFeatures/MainRoleAndRoleFeatures/Commands/RemoveById/RemoveByIdMRARFValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace OMPS.ApplicationKatmanı.Features.Commands.AppFeatures.MainRoleAndRoleFeatures.Commands.RemoveById
+{
+    public sealed class RemoveByIdMRARFValidator : AbstractValidator<RemoveByIdMRARFCommand>
+    {
+        public RemoveByIdMRARFValidator()
+        {
+            RuleFor(p => p.id).NotNull().WithMessage("id  bilgisi yazılmalıdır!");
+            RuleFor(p => p.id).NotEmpty().WithMessage("id bilgisi yazılmalıdır!");
+
+        }
+    }
+}
